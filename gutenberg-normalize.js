e2008328546168126c0beb4e9d2c2814b706a17d
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-12-01 11:29:02 -0700

    feat: extract title and author

--- gutenberg-normalize.js
+++ gutenberg-normalize.js
@@ -4,12 +4,28 @@ const globby = require("globby");
 const unified = require("unified");
 const rehype = require("rehype-parse");
 const santize = require("rehype-sanitize");
-const toVfile = require("to-vfile");
+const { readSync: toVfile } = require("to-vfile");
 const { writeFile } = require("fs").promises;
 const { basename } = require("path");
+const { select, selectAll } = require("hast-util-select");
 
 const simplifier = () => ast => {
-  return ast;
+  const metadataNode = select("pre", ast);
+  let author, title;
+  if (metadataNode) {
+    const metadataText = metadataNode.children[0].value;
+    title = /title:\w*(.+)/i.exec(metadataText);
+    author = /author:\w*(.+)/i.exec(metadataText);
+
+    if (title) title = title[1].trim();
+    if (author) author = author[1].trim();
+  }
+
+  return {
+    type: "book",
+    author,
+    title
+  };
 };
 
 (async () => {
@@ -19,10 +35,17 @@ const simplifier = () => ast => {
   const processor = unified()
     .use(rehype) // parse HTML
     .use(santize) // remove fluff content
-    .use(simplifier)
-    .freeze();
+    .use(simplifier); // reformat to new tree format
+
+  processor.Compiler = JSON.stringify;
+  processor.freeze();
+
   for (const p of paths) {
-    const content = processor.parse(toVfile.readSync(p));
-    await writeFile("json/" + basename(p) + ".json", JSON.stringify(content));
+    try {
+      const content = await processor.process(toVfile(p));
+      await writeFile("json/" + basename(p) + ".json", content);
+    } catch (err) {
+      console.error(err);
+    }
   }
 })();
